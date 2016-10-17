b61af61bf8f7483b036d8a62d7f646a700ef5c13
Jonas Wagner <boom@confuflux.com>
2016-10-17 13:42:35 -0600

    n/c

--- modules/chat.py
+++ modules/chat.py
@@ -9,29 +9,35 @@ CLIENT_ACCESS_TOKEN = ''
 nowords = ['reload', 'help', 'tell', 'ask', 'ping']
 
 def chat(phenny, input):
+    nick = input.group()
     text = input.group(2)
+
     for x in nowords:
         if text.startswith(x):
             return
 
-    if text.startswith('.'):
-        return
-
     if not CLIENT_ACCESS_TOKEN:
         return phenny.say('Please set the client access token first to use this feature.')
 
-    ai = apiai.ApiAI(CLIENT_ACCESS_TOKEN)
-    request = ai.text_request()
-    request.lang = 'en'
-    request.query = text
+    if text.startswith('.'):
+        return
 
-    response = request.getresponse()
-    jsondata = json.loads(response.read().decode())
-    status = jsondata['status']['code']
+    if nick.startswith(phenny.config.nick + ':'):
+        ai = apiai.ApiAI(CLIENT_ACCESS_TOKEN)
+        request = ai.text_request()
+        request.lang = 'en'
+        request.query = text
 
-    if status == 200:
-        phenny.say(jsondata['result']['fulfillment']['speech'])
-    else:
-        phenny.say(jsondata['status']['errorDetails'])
+        response = request.getresponse()
+        jsondata = json.loads(response.read().decode())
+        status = jsondata['status']['code']
+
+        if status == 200:
+            phenny.say(jsondata['result']['fulfillment']['speech'])
+        else:
+            phenny.say(jsondata['status']['errorDetails'])
 
 chat.rule = r'(?i)($nickname[:,]?\s)?(.*)'
+
+if __name__ == "__main__":
+    print(__doc__.strip())
