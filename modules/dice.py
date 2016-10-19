7520c61037113301b72ef2bce8eaf7c71a7861f0
Jonas Wagner <boom@confuflux.com>
2016-10-19 10:09:10 -0600

    dice dice baby

--- modules/dice.py
+++ modules/dice.py
@@ -1,6 +1,17 @@
 import random
 
 def dice(phenny, input):
-    return phenny.say(str(random.randint(1,6)))
+    random.seed()
 
+    if not input.group(2):
+        return phenny.say(str(random.randint(1,6)))
+
+    count = 1
+    c = []
+    while count <= int(input.group(2)):
+        rand = str(random.randint(1,6))
+        c.append(rand)
+        count += 1
+
+    return phenny.say(' '.join(c))
 dice.commands = ['dice']
